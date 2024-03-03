e3ea403de40e56f138f864578946f8917469e4ba
Nguyễn Đức Anh <99268985+GLNihilis@users.noreply.github.com>
2024-03-03 23:20:38 +0700

    update

--- Assets/Script/PlateKitchenObject.cs
+++ Assets/Script/PlateKitchenObject.cs
@@ -42,4 +42,9 @@ public class PlateKitchenObject : KitchenObject
             return true;
         }
     }
+
+    public List<KitchenObjectsSO> GetKitchenObjectsSOList()
+    {
+        return kitchenObjectsSOList;
+    }
 }
